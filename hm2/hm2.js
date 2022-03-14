d611c0556624e2c7467aa9606a52e1e0a7b2ecd3
Leonid (notebook) <leonidrawhide@gmail.com>
2022-03-14 23:20:03 +0300

    error fix

---
+++ hm2/hm2.js
@@ -0,0 +1,72 @@
+// /**
+//  * 1 Задание - создаем Map и ключами от 0 до 100 и значениями null
+//  */
+// const mapOfNumbers = new Map<number, null>()
+// for (let i = 0; i <= 100; i++) mapOfNumbers.set(i, null)
+// console.log(mapOfNumbers)
+// /**
+//  * 2 Задание - проверяем соответствие ключей Map'ы условиям: 
+//  * делится на 3, на 5 и на оба числа
+//  */
+// mapOfNumbers.forEach((val, key) => {
+// 	if (key != 0 && (key % 3 == 0 || key % 5 == 0)) {
+// 		console.log(key % 3 != 0 ? `${key} делится на 5` : 
+// 					key % 5 != 0 ? `${key} делится на 3` : 
+// 					`${key} делится как на 3, так и на 5`)
+// 	}
+// })
+// /**
+//  * 3 Задание - проверям список домов на соответствие условиям
+//  */
+// const listOfHouses: string[][] = [
+// 	["36", "не приватизирован", "4 собственника", "Парковая"],
+// 	["45", "приватизирован", "3 собственника", "Луговая"],
+// 	["56", "не приватизирован", "1 собственник", "Луговая"],
+// 	["56", "приватизирован", "5 собственников", "Ленина"],
+// 	["25", "не приватизирован", "2 собственника", "Ленина"]
+// ]
+// listOfHouses.forEach((house) => {
+// 	const [area, street] = [+house[0], house[3]]
+// 	console.log(checkHouse(area, street))
+// })
+// function checkHouse(area: number, street: string): boolean {
+// 	return(area < 56 && area > 40 ? false : 
+// 		area >= 56 || street == `Ленина` ? true : false)
+// }
+// =====party1======/
+var mapFor = new Map();
+for (var i = 0; i <= 100; i++) {
+    mapFor.set(i, null);
+}
+//========party2=======//
+mapFor.forEach(function (value, key) {
+    console.log(value);
+    if (key % 3 === 0) {
+        console.log("".concat(key, " divaded the 3"));
+    }
+    if (key % 5 === 0) {
+        console.log("".concat(key, " oops here is 5"));
+    }
+    else if (key % 3 === 0 && key % 5 === 0) {
+        console.log("".concat(key, " i can beelive that"));
+    }
+});
+//===========party3=========//
+var arr = [["36", "не приватизирован", "4 собственника", "Парковая"],
+    ["45", "приватизирован", "3 собственника", "Луговая"],
+    ["56", "не приватизирован", "1 собственник", "Луговая"],
+    ["56", "приватизирован", "5 собственников", "Ленина"],
+    ["25", "не приватизирован", "2 собственника", "Ленина"]];
+arr.forEach(function (item) {
+    var numHome = Number(item[0]);
+    var streetHome = item[3];
+    console.log(getHome(streetHome, numHome));
+});
+function getHome(streetHome, numHome) {
+    if ((numHome <= 40 && streetHome === "Ленина") || (numHome >= 56)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
--- hm2/hm2.ts
+++ hm2/hm2.ts
@@ -41,3 +41,46 @@ function checkHouse(area: number, street: string): boolean {
 	return(area < 56 && area > 40 ? false : 
 		area >= 56 || street == `Ленина` ? true : false)
 }
+
+// // =====party1======/
+// const mapFor = new Map()
+
+// for(let i = 0; i <= 100; i++){
+//     mapFor.set(i, null)
+// }
+
+// //========party2=======//
+
+// mapFor.forEach((value, key)=>{
+//     console.log(value)
+//     if(key % 3 === 0){
+//         console.log(`${key} divaded the 3`)
+//     }
+//     if(key % 5 === 0){
+//         console.log(`${key} oops here is 5`)
+//     }else if(key % 3 === 0 && key % 5 === 0){
+//         console.log(`${key} i can beelive that`);
+//     }
+// })
+
+// //===========party3=========//
+
+// const arr = [["36", "не приватизирован", "4 собственника", "Парковая"],
+// ["45", "приватизирован", "3 собственника", "Луговая"],
+// ["56", "не приватизирован", "1 собственник", "Луговая"],
+// ["56", "приватизирован", "5 собственников", "Ленина"],
+// ["25", "не приватизирован", "2 собственника", "Ленина"]]
+
+// arr.forEach((item) => {
+//     let numHome = Number(item[0])
+//     let streetHome = item[3]
+//     console.log(getHome(streetHome, numHome ))
+// })
+
+// function getHome(streetHome: string, numHome: number){
+//     if ((numHome <= 40 && streetHome === "Ленина") || (numHome >= 56)) {
+//         return true
+//     }else{
+//         return false
+//     }
+// }
